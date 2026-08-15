b5f64b4f87003ea525b6b4de412815b91dd0332e
Jayasree Sundarasekar <jaysde26@gmail.com>
2026-08-15 12:08:06 +0530

    Create 01_personal_info.py

---
+++ 01_personal_info.py
@@ -0,0 +1,12 @@
+# Personal Information Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college name: ")
+goal = input("Enter your career goal: ")
+
+print("\n--- Personal Information ---")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Career Goal:", goal)
